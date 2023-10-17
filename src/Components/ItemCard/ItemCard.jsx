aabcaa8f3ef5c2d0e566cfe17c989373bbef9edc
German Ravarotto <g44ravarotto@gmail.com>
2023-10-17 19:35:35 -0300

    router install

---
+++ src/Components/ItemCard/ItemCard.jsx
@@ -0,0 +1,29 @@
+import { Card, CardFooter, CardHeader, Image, Button } from "@nextui-org/react"
+
+const ItemCard = ({ producto }) => {
+    return (
+        <Card isFooterBlurred className="w-full h-[320px] hover:scale-105 ">
+            <CardHeader className="absolute z-10 top-1 flex-col items-start">
+                <p className="text-sm text-white/60 uppercase font-bold">New</p>
+                <h4 className="text-danger font-medium text-2xl">{producto.title}</h4>
+            </CardHeader>
+            <Image
+                removeWrapper
+                alt="Card example background"
+                className="z-0 w-full h-full scale-100 -translate-y-6 object-cover"
+                src={producto.image}
+            />
+            <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
+                <div>
+                    <p className="text-black text-lg">{producto.price}</p>
+                    <p className="text-black text-tiny">{producto.category}</p>
+                </div>
+                <Button className="text-tiny" color="primary" radius="full" size="sm">
+                    Comprar
+                </Button>
+            </CardFooter>
+        </Card>
+    )
+}
+
+export default ItemCard
--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -1,11 +1,33 @@
+import { useEffect, useState } from "react"
 import styles from "../ItemListContainer/Styles.module.css"
+import ItemCard from "../ItemCard/ItemCard"
 
-function ItemListContainer({greetings}) {
+function ItemListContainer({ greetings }) {
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products?limit=8')
+            .then(res => res.json())
+            .then(data => {
+                console.table(data)
+                setProducts(data)
+            })
+    }, [])
+
     return (
         <div className={styles.itemListContainer}>
-            <h1 className={styles.greetings}>{greetings}</h1>
+            <div  className="w-full h-auto flex justify-center items-center ">
+                <h1 id="greetings" className="font-['Open_Sans'] text-[2rem] sm:text-[5rem] text-warning hover:text-danger hover:scale-100">{greetings}</h1>
+            </div>
+            <div className="w-3/4 gap-10 grid grid-cols-1 sm:grid-cols-4 p-10 ">
+                {products.map((product) => (
+                    <ItemCard key={product.id} producto={product} />
+                ))
+                }
+            </div>
         </div>
     )
 }
-
-export default ItemListContainer
+// {styles.titleContainer}
+export default ItemListContainer;
